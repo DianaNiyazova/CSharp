de6e34760a73463cb3bcc3d30e4d4fe234a94b28
DianaNiyazova <anotherdeadangel@mail.ru>
2022-11-04 19:01:01 +0800

    приветствие

---
+++ example5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя - я: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "диана")
+{
+    Console.WriteLine("OMG, это же ДИВАНА");
+}
+else
+{
+    Console.Write("Привет, мазафака "); Console.WriteLine(username);
+}
